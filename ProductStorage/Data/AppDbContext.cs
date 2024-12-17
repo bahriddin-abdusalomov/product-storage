34fff1ed15177b4b4a885fb06b8356c4eef52e36
Bahriddin Abdusalomov <123171397+bahriddin-abdusalomov@users.noreply.github.com>
2024-12-17 21:02:43 +0500

    asda

--- ProductStorage/Data/AppDbContext.cs
+++ ProductStorage/Data/AppDbContext.cs
@@ -11,6 +11,6 @@ public class AppDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseNpgsql("Host=localhost; Database=product-storage; User ID=postgres; Port=5432; Password=1234;");
+        optionsBuilder.UseNpgsql("Host=localhost; Database=product-storage; User ID=postgres; Port=5432; Password=0809;");
     }
 }
